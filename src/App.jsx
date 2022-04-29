7e358806be5f0e2a8004df46486bb12198c688ae
Sefton Dockstader <sefdock@gmail.com>
2022-04-29 11:25:22 -0600

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/home';
+import Nba from './routes/Nba';
 import './App.css';
+import Nfl from './routes/Nfl';
+import Mlb from './routes/Mlb';
 
 function App() {
   return (
-    <p>
-      Cleanedup React App
-    </p>
+    <>
+      <Routes>
+        <Route path="/" element={<Home/>}>
+          <Route path="nba" element={<Nba/>}/>
+          <Route path="nfl" element={<Nfl/>}/>
+          <Route path="mlb" element={<Mlb/>}/>
+        </Route>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/routes/Mlb.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Mlb() {
+  return (
+    <div>Mlb</div>
+  )
+}
+
+export default Mlb
